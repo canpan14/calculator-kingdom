33eb4e6f03191ab0bae6f73ba617b300f90da7a3
canpan14 <sammachlin@hotmail.com>
2017-11-16 15:27:56 -0500

    Initial Commit
    Setup with Bootstrap 4 and Font-Awesome

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
+
+// load manifests
+// bootstrap 4
+window.Popper = require('popper.js')
+require('bootstrap')
+
+// scripts
+require('./assets/scripts/index.js')
+
+// styles
+require('./assets/styles/index.scss')
